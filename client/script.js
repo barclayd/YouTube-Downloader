a79a6ed56eeb7a620fcd6bfe207495313f798433
Daniel Barclay <barclaysd@me.com>
2019-05-23 23:17:18 +0100

    Added server functionality and JavaScript to validate and check YouTube URL. Video downloads locally after processing request

---
+++ client/script.js
@@ -0,0 +1,14 @@
+const convertButton = document.querySelector('.button-convert');
+const inputURL = document.querySelector('.input-url');
+
+const serverURL = 'http://localhost:8000';
+
+convertButton.addEventListener('click', () => {
+   if (inputURL.value.trim() !== null) {
+       sendButton(inputURL.value);
+   }
+});
+
+const sendButton = (videoURL) => {
+    window.location.href = `${serverURL}/download?URL=${videoURL}`;
+};
--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import ytdl from 'ytdl-core';
 
 const express = require('express');
 
@@ -6,7 +7,13 @@ const app = express();
 
 app.use(cors('*'));
 
+app.get('/download', (req, res) => {
+  const { URL } = req.query;
+  res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+  ytdl(URL, {
+    format: 'mp4',
+  }).pipe(res);
+});
+
 // eslint-disable-next-line no-console
-app.listen({ port: 8000 }, () => console.log(
-  '🚀 Server ready at http://localhost:8000',
-));
+app.listen({ port: 8000 }, () => console.log('🚀 Server ready at http://localhost:8000'));
