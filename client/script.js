51066de8bb023ce62651ef6aa78dfc80fb9d2b4c
Daniel Barclay <barclaysd@me.com>
2019-05-24 21:03:27 +0100

    Save a video as video only, audio only or audio&video and configure quality

--- client/script.js
+++ client/script.js
@@ -1,14 +1,18 @@
 const convertButton = document.querySelector('.button-convert');
 const inputURL = document.querySelector('.input-url');
+const format = document.getElementById('format');
+const quality = document.getElementById('quality');
 
 const serverURL = 'http://localhost:8000';
 
 convertButton.addEventListener('click', () => {
-   if (inputURL.value.trim().length > 0) {
-       sendButton(inputURL.value);
-   }
+  const selectedFormat = format[format.selectedIndex].value;
+  const selectedQuality = quality[quality.selectedIndex].value;
+  if (inputURL.value.trim().length > 0) {
+    sendButton(inputURL.value, selectedFormat, selectedQuality);
+  }
 });
 
-const sendButton = (videoURL) => {
-    window.location.href = `${serverURL}/download?URL=${videoURL}`;
+const sendButton = (videoURL, format, quality) => {
+  window.location.href = `${serverURL}/download?URL=${videoURL}&downloadFormat=${format}&quality=${quality}`;
 };
