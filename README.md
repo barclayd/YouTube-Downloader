c4b0b875329de1907705a6955da889ff835b0531
Daniel Barclay <daniel@urbanintel.io>
2019-05-24 22:52:31 +0100

    Tweaked styling and updated README.md

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
-# YouTube Downloader
-Download any YouTube video. Built using Node.js
+# YouTube Downloader & Converter
+
+Download and convert any YouTube video to MP3 or MP4. Built using Vanilla JS and Node.js
+
+***N.b. Only to be used in accordance with YouTube's [terms of service](https://www.youtube.com/static?gl=GB&template=terms)***
+
+### Code style
+  [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+    [![js-standard-style](https://img.shields.io/badge/deployed-heroku-blue.svg)](https://stormy-reaches-60483.herokuapp.com/)
+    [![js-standard-style](https://img.shields.io/badge/deployed%20version-1.0.0-green.svg)](https://stormy-reaches-60483.herokuapp.com/)
+
+# Demo
+
+<p align="center">
+    <img alt="Walkthrough" src='https://user-images.githubusercontent.com/39765499/58358323-52afbb80-7e76-11e9-87f6-af65bae7ca34.gif'>
+
+<img width="1112" alt="Screenshot 2019-05-24 at 22 38 30" src="https://user-images.githubusercontent.com/39765499/58357975-d49ee500-7e74-11e9-939d-d7ac314c11f4.png">
+
+</p>
+
+# Features
+
+- [x] Select video and audio quality
+- [x] Select audio only (YouTube to MP3)
 
 # Future Improvements
 
-* Advanced options:
-    * start time
-* Select video and audio quality
-* Select audio only (YouTube to MP3)
-* Configure format
+* Configure format (e.g. flv, wmv)
+* Specify start and end times for video
+* Batch convert videos
+
     
--- client/script.js
+++ client/script.js
@@ -6,23 +6,27 @@ const quality = document.getElementById('quality');
 const serverURL = 'http://localhost:8000';
 
 convertButton.addEventListener('click', () => {
-  const selectedFormat = format[format.selectedIndex].value;
-  const selectedQuality = quality[quality.selectedIndex].value;
-  if (inputURL.value.trim().length > 0) {
-    sendButton(inputURL.value, selectedFormat, selectedQuality);
-  }
+    const selectedFormat = format[format.selectedIndex].value;
+    const selectedQuality = quality[quality.selectedIndex].value;
+    if (inputURL.value.trim().length > 0) {
+        sendButton(inputURL.value, selectedFormat, selectedQuality);
+    }
 });
 
 const sendButton = (videoURL, format, quality) => {
-  fetch(`${serverURL}/check-download?URL=${videoURL}`)
-      .then(response => {
-        return response.json();
-      })
-      .then(resData => {
-        const data = JSON.parse(JSON.stringify(resData));
-        if (data.status === true) {
-          document.getElementById("downloading").innerHTML = `Starting the download of ${data.title} by ${data.author}...`;
-          window.location.href = `${serverURL}/download?URL=${videoURL}&downloadFormat=${format}&quality=${quality}&title=${data.title}`;
-        }
-      });
+    fetch(`${serverURL}/check-download?URL=${videoURL}`)
+        .then(response => response.json())
+        .then(resData => {
+            const data = JSON.parse(JSON.stringify(resData));
+            if (data.status === true) {
+                document.getElementById(
+                    'downloading',
+                ).innerHTML = `Starting the download of ${data.title} by ${
+                    data.author
+                    }...`;
+                window.location.href = `${serverURL}/download?URL=${videoURL}&downloadFormat=${format}&quality=${quality}&title=${
+                    data.title
+                    }`;
+            }
+        });
 };
