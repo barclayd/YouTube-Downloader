3734e4c330fe8d1879d383fb232cc5d95851e23c
Daniel Barclay <barclaysd@me.com>
2020-03-26 09:59:28 +0000

    Handle errors in try/catch

--- server/app.js
+++ server/app.js
@@ -8,42 +8,50 @@ const app = express();
 app.use(cors('*'));
 
 app.get('/check-download', async (req, res, next) => {
-  const { URL } = req.query;
-  const {
-    player_response: {
-      videoDetails: { title, author },
-    },
-  } = await ytdl.getBasicInfo(URL);
-  res.json({
-    status: true,
-    title,
-    author,
-  });
-  next();
+  try {
+    const { URL } = req.query;
+    const {
+      player_response: {
+        videoDetails: { title, author },
+      },
+    } = await ytdl.getBasicInfo(URL);
+    res.json({
+      status: true,
+      title,
+      author,
+    });
+    next();
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 app.get('/download', async (req, res) => {
-  const {
-    URL, downloadFormat, quality, title,
-  } = req.query;
-  if (downloadFormat === 'audio-only') {
-    res.setHeader(
-      'Content-Disposition',
-      `attachment; filename=${title.substring(0, 40)}.mp3`,
-    );
-    ytdl(URL, {
-      filter: format => format.container === 'm4a' && !format.encoding,
-      quality: quality === 'high' ? 'highest' : 'lowest',
-    }).pipe(res);
-  } else {
-    res.header(
-      'Content-Disposition',
-      `attachment; filename=${title.substring(0, 25)}.mp4`,
-    );
-    ytdl(URL, {
-      filter: downloadFormat === 'video-only' ? 'videoonly' : 'audioandvideo',
-      quality: quality === 'high' ? 'highestvideo' : 'lowestvideo',
-    }).pipe(res);
+  try {
+    const {
+      URL, downloadFormat, quality, title,
+    } = req.query;
+    if (downloadFormat === 'audio-only') {
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=${title.substring(0, 40)}.mp3`,
+      );
+      ytdl(URL, {
+        filter: format => format.container === 'm4a' && !format.encoding,
+        quality: quality === 'high' ? 'highest' : 'lowest',
+      }).pipe(res);
+    } else {
+      res.header(
+        'Content-Disposition',
+        `attachment; filename="${title.substring(0, 25)}.mp4"`,
+      );
+      ytdl(URL, {
+        filter: downloadFormat === 'video-only' ? 'videoonly' : 'audioandvideo',
+        quality: quality === 'high' ? 'highestvideo' : 'lowestvideo',
+      }).pipe(res);
+    }
+  } catch (e) {
+    console.log(e);
   }
 });
 
